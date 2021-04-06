815e79805ee679d343bb14e01688c786fd7bb890
Raphael Augusto <raphaelaugusto@tutanota.com>
2021-04-06 13:44:46 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Amém senhor
+          Amém senhor!
+          
         </p>
         <a
           className="App-link"
